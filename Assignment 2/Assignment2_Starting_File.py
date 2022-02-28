06b84f3c3196322dcbb2649bf64f3776a1c7795a
NickTheWhale <loehrketurkey7@gmail.com>
2022-02-28 10:29:14 -0600

    first commit

---
+++ Assignment 2/Assignment2_Starting_File.py
@@ -0,0 +1,33 @@
+"""
+Name:       <Fill in your name>
+
+Course:     CS1430, Section <Insert section here>,  Spring 2022
+
+Assignment: Assignment 2
+
+Purpose:    
+
+Input:      
+
+Output:     
+
+"""
+
+##################################
+#CONSTANTS
+##################################
+
+
+
+def main():
+    """
+    This is your docstring - replace this with a description of the program.
+    :return:
+    """
+    # remove the word pass below. All of your code should be here in main().
+    pass
+
+#Do not remove the code below this line!
+
+if __name__ == '__main__':
+    main()
