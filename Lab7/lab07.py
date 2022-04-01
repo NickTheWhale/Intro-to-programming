de4513109bef9a30e1a0536e945ba638b937dfc9
NickTheWhale <loehrketurkey7@gmail.com>
2022-04-01 10:54:57 -0500

    15/20

--- Lab7/lab07.py
+++ Lab7/lab07.py
@@ -59,7 +59,7 @@ def main():
 
     :return:
     """
-    random.seed(_RNG)
+    random.seed(_SEED)
     phase = input("run phase: true or false: ")
     phase = phase.upper()
     if phase == "TRUE":
@@ -72,6 +72,9 @@ def main():
         pass
     elif phase == "TEST":
         print(get_font_color("yellow", False))
+        main()
+    elif phase == "TIMED":
+        run_phase_timed(False)
         main()
     else:
         print("invalid input")
@@ -98,6 +101,37 @@ def run_phase(is_phase_1):
         if run_single_test(is_phase_1):
             n_correct += 1
     return n_correct
+
+
+def run_phase_timed(is_phase_1):
+    n_correct = 0
+    test_count = 0
+    previous_answer_time = time.time()
+    while time.time() - previous_answer_time <= 10:
+        if not is_phase_1:
+            name_color = random_color_name()
+            font_color = random_color_name()
+            while font_color == name_color:
+                font_color = random_color_name()
+            print_in_color(name_color, font_color)
+            previous_time = time.time()
+            answer_color = input("What color ink is the word written in? ")
+            if answer_color != font_color:
+                current_time = time.time()
+                print(f'Correct answer was {name_color}.')
+            else:
+                current_time = time.time()
+                if current_time - previous_answer_time <= 10:
+                    n_correct += 1
+            interval = current_time - previous_time - _TIME_OFFSET
+            interval *= 1000
+            interval = round(interval)
+            test_count += 1
+            print(f'Time: {interval} milliseconds')
+    print('')
+    print('')
+    print(f'You got {n_correct} out of {test_count} in 10 seconds')
+
 
 
 def run_single_test(is_phase_1):
